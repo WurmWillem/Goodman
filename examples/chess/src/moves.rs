dc6c33f4fa58f08f5c409e8badcecc03e904412e
WurmWillem <84849889+WurmWillem@users.noreply.github.com>
2024-04-24 22:09:29 +0200

    clean up

--- examples/chess/src/moves.rs
+++ examples/chess/src/moves.rs
@@ -3,14 +3,21 @@ use crate::types::{Kind, Piece, Side};
 pub fn make_move(pieces: &mut Vec<Vec<Piece>>, index: (usize, usize), m: (usize, usize)) {
     let mut orig_piece = pieces[index.0][index.1].clone();
 
-    // remove pawn if en passant was done
-    if matches!(orig_piece.kind, Kind::Pawn(_)) && m.1 != 0 {
-        pieces[index.0][m.1] = Piece::new_empty();
-    }
+    if matches!(orig_piece.kind, Kind::Pawn(_)) {
+        // remove pawn if en passant was done
+        if pieces[m.0][m.1].kind == Kind::None {
+            pieces[index.0][m.1] = Piece::new_empty();
+        }
 
-    // make pawn true if it moved 2 spaces forward
-    if matches!(orig_piece.kind, Kind::Pawn(_)) && (m.0 as i32 - index.0 as i32).abs() == 2 {
-        orig_piece.kind = Kind::Pawn(true);
+        // make pawn true if it moved 2 spaces forward
+        if (m.0 as i32 - index.0 as i32).abs() == 2 {
+            orig_piece.kind = Kind::Pawn(true);
+        }
+        
+        // make pawn a queen if it crossed the board
+        if m.0 == 7 || m.0 == 0 {
+            orig_piece = Piece::new(Kind::Queen, orig_piece.side);
+        }
     }
 
     pieces[m.0][m.1] = orig_piece;
